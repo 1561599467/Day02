b29ed41b40db0390296fd556627684bae6480860
DELL <1561599467@qq.com>
2018-12-02 13:35:48 +0800

    练习

--- src/Demo/Demo_1.java
+++ src/Demo/Demo_1.java
@@ -2,7 +2,7 @@ package Demo;
 
 public class Demo_1 {
     public static void main(String[] args) {
-        short s = (short) 12;
+        short s = (short) 13;
         System.out.println(s);
     }
 }
